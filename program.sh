09aa3b16163243015f9ad2e8b7d328c350673700
sollo93 <49595811+sollo93@users.noreply.github.com>
2019-04-26 19:51:19 +0300

    1
    111

---
+++ program.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+sudo pacman -Syy  gimp hexchat firefox firefox-i18n-ru pidgin screenfetch ufw ntfs-3g vlc file-roller p7zip unrar gvfs pulseaudio 
+yay -S sylpheed cherrytree purple-vk-plugin hunspell-ru pamac-aur xflux xflux-gui-git sublime-text
